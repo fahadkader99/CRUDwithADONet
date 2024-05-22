0713bc7e1f0a910f5c32ab4f8e93d2768cb1c027
Fahad Kader <fahadkader786@outlook.com>
2024-05-21 20:51:12 -0400

    Add project files.

---
+++ CRUDwithADONet.Web/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUDwithADONet.Web.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public double Salary { get; set; }
+
+    }
+}
---
+++ CRUDwithADONet.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CRUDwithADONet.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
